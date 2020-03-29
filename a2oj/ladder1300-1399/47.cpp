4b57aa9d5a6b25372d7b1373b58eb2ec6b67309a
harkiratdhanoa <harkirat2016@gmail.com>
2020-03-29 22:22:09 +0530

    a2oj ladder1300-1399 Q47

--- a2oj/ladder1300-1399/47.cpp
+++ a2oj/ladder1300-1399/47.cpp
@@ -4,24 +4,30 @@ using namespace std;
 #define forn(i,n) for(int i=0;i<n;i++)
 
 int main(){
-	int n;
+	int n,q,type,l,r;
 	cin>>n;
-	vector<int> a(n,0),b(n,0);
+	vector<long long> a(n,0),b(n,0);
 	forn(i,n){
 		cin>>b[i];
-		a[i]=b[i]+a[i-1];
+		if(i==0)
+			a[0]=b[0];
+		else
+			a[i]=b[i]+a[i-1];
 	}
-	a[0]=b[0];
+	
 	sort(b.begin(),b.end());
 
 	for(int i=1;i<n;i++){
 		b[i]+=b[i-1];
 	}
-	int q;
 	cin>>q;
-	forn(i,q){
-		if(i==1)
-			cout<<a[r]-a[l-1];
+	while(q--){
+		cin>>type>>l>>r;
+		l--;r--;
+		if(type==1)
+			cout<<a[r]-(l>0?a[l-1]:0)<<"\n";
+		else
+			cout<<b[r]-(l>0?b[l-1]:0)<<"\n";
 	}
 	return 0;
 } 
