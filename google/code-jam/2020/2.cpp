03f5c358c3282f50979be4ed0677092c2e8f3024
harkiratdhanoa <harkirat2016@gmail.com>
2020-04-04 18:06:48 +0530

    google codejam Qualification Round 2020

--- google/code-jam/2020/2.cpp
+++ google/code-jam/2020/2.cpp
@@ -3,28 +3,27 @@ using namespace std;
 #define forn(i, n) for(int i = 0; i < n; ++i)
 typedef pair<int, int> pi;
 
+string f(string s,int x){
+	if(s.length()==0)
+		return "";
+	int mini=0,n=s.length();
+	forn(i,n)
+		mini=s[mini]>s[i]?i:mini;
+	string L="",R="";
+	forn(i,s[mini]-'0'-x){
+		L+='(';
+		R+=')';
+	}
+	return L+f(s.substr(0,mini),s[mini]-'0')+s[mini]+f(s.substr(mini+1,n-mini-1),s[mini]-'0')+R;
+}
 int main(){
-	int t,n,b,cnt=0,tot,test;
+	int t,test,n;
+	string s;
 	cin>>test;
 	t=test;
 	while(t--){
-		cin>>n>>b;
-		cnt=0;
-		tot=0;
-		vector<int> a(n);
-		forn(i,n){
-			cin>>a[i];
-		}
-		sort(a.begin(),a.end());
-		forn(i,n){
-			tot+=a[i];
-			
-			if(tot>b){
-				break;
-			}
-			cnt+=1;
-		}
-		cout<<"Case #"<<test-t<<": "<<cnt<<"\n";
+		cin>>s;
+		cout<<"Case #"<<test-t<<": "<<f(s,0)<<"\n";
 	    
 	}
 	return 0;
---
+++ google/code-jam/2020/4.py
@@ -0,0 +1,43 @@
+test,b=[int(x) for x in input().split()]
+t=test
+
+def opp(string s):
+    x=""
+    for i in s:
+        if i=='0':
+            x+='1'
+        if i=='1'
+            x+='0'
+    return x
+
+while t and b==10:
+    t-=1
+    s=''
+    for i in range(10):
+        print(i+1)
+        s+=input()
+
+    print(s)
+    verdict=input()
+    if verdict!="Y" :
+        break
+
+while t and b==20:
+    t-=1
+    left,mid,right='','',''
+    for i in range(10):
+        print(i+1)
+        left+=input()
+
+    for i in range(10):
+        print(i+6)
+        mid+=input()
+
+    if left[6:]==mid[:6]==mid[6:][::-1]:
+        
+
+    print(s)
+    verdict=input()
+    if verdict!="Y" :
+        break
+        
