d709589ba7f22da0f4f0ebd3c459c158a154e319
harkiratdhanoa <harkirat2016@gmail.com>
2020-04-12 22:34:42 +0530

    codeforces: round 633 (Div 2)

---
+++ codeforces/#633/a.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i, n) for(int i = 0; i < n; ++i)
+ 
+int main(){
+	int t,a,b,n,ai,bi;
+	bool bo;
+
+	cin>>t;
+
+	while(t--){
+		cin>>n;
+		cout<<n<<"\n";
+	}
+	return 0;
+}
