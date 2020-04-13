b24144721a79c1b7a75b62d08c84b8a60418ebac
harkiratdhanoa <harkirat2016@gmail.com>
2020-04-13 23:43:38 +0530

    codeforces: adding script to create new contest directory

--- codeforces/contest.sh
+++ codeforces/contest.sh
@@ -1,21 +1,36 @@
-mkdir $1
-cd $1
-open .
-echo "#include<bits/stdc++.h>
-using namespace std;
-#define forn(i, n) for(int i = 0; i < n; ++i)
+cd /Users/harkiratdhanoa/Desktop/iitd/competitive/codeforces
+if [ $# -eq 0 ]; 
+then
+	open .
+	echo "No arguments provided"
+else
+	mkdir $1
+	cd $1
 
-int main(){
-	ios_base::sync_with_stdio(false); 
-    cin.tie(NULL);   
-	int n,t;
-	cin>>t;
 
-	while(t--){
-		cin>>n;
-		
-		cout<<n<<"\n";
+	echo "#include<bits/stdc++.h>
+	using namespace std;
+	#define forn(i, n) for(int i = 0; i < n; ++i)
+	#define forit(it, m) for(auto it = m.begin(); it!=m.end(); ++it)
+
+	int main(){
+		ios_base::sync_with_stdio(false); 
+	    	cin.tie(NULL);   
+		int n,t;
+		cin>>t;
+	
+		while(t--){
+			cin>>n;
+			
+			cout<<n<<"\n";
+		}
+		return 0;
 	}
-	return 0;
-}
-" >> a.cpp >> b.cpp
+	" | tee -a a.cpp b.cpp c.cpp d.cpp
+
+	open .
+
+	# subl a.cpp
+
+	clear
+fi
