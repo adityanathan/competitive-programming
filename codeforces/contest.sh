4f79b6ce3f505bc52b76092963f65b11c4202cc9
harkiratdhanoa <harkirat2016@gmail.com>
2020-04-14 23:30:30 +0530

    codeforces: adding script to create new contest directory

--- codeforces/contest.sh
+++ codeforces/contest.sh
@@ -14,8 +14,8 @@ using namespace std;
 #define forit(it, m) for(auto it = m.begin(); it!=m.end(); ++it)
 
 int main(){
-	ios_base::sync_with_stdio(false); 
-    cin.tie(NULL);   
+	ios::sync_with_stdio(false);
+  	cin.tie(0); 
 	int n,t;
 	cin>>t;
 
