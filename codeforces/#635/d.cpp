fb0820bd59ecd7a7b29e7c351446930c56971986
harkiratdhanoa <harkirat2016@gmail.com>
2020-04-17 23:51:45 +0530

    codeforces: round 635 (Div 2) ABD

--- codeforces/#635/d.cpp
+++ codeforces/#635/d.cpp
@@ -1,70 +1,52 @@
 #pragma GCC optimize (Ofast)
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
 #include<bits/stdc++.h>
 #include<unordered_map>
 using namespace std;
+
 #define forn(i, n) for(int i = 0; i < n; ++i)
 #define forit(it, m) for(auto it = m.begin(); it!=m.end(); ++it)
 
-long long min(float a, float b){
-	if(a<b)
-		return (long long)a;
-	return (long long)b;
-}
-long long getClosest(long long val1, long long val2, 
-               long long target) 
-{ 
-    if (target - val1 >= val2 - target) 
+long long getClosest(long long val1, long long val2, long long target){ 
+    if(target - val1 >= val2 - target) 
         return val2; 
     else
         return val1; 
 } 
-long long findClosest(vector<long long> arr, long long n, long long target) 
-{ 
-    // Corner cases 
-    if (target <= arr[0]) 
+
+long long cal(long long a, long long b, long long c){
+	return (a-b)*(a-b)+(b-c)*(b-c)+(c-a)*(c-a);
+}
+
+long long findClosest(vector<long long> arr, long long n, long long target){ 
+    if(target <= arr[0]) 
         return arr[0]; 
-    if (target >= arr[n - 1]) 
+    if(target >= arr[n - 1]) 
         return arr[n - 1]; 
-  
-    // Doing binary search 
     int i = 0, j = n, mid = 0; 
-    while (i < j) { 
+    while(i < j){ 
         mid = (i + j) / 2; 
-  
-        if (arr[mid] == target) 
+        if(arr[mid] == target) 
             return arr[mid]; 
-  
-        /* If target is less than array element, 
-            then search in left */
-        if (target < arr[mid]) { 
-  
-            // If target is greater than previous 
-            // to mid, return closest of two 
-            if (mid > 0 && target > arr[mid - 1]) 
-                return getClosest(arr[mid - 1], 
-                                  arr[mid], target); 
-  
-            /* Repeat for left half */
-            j = mid; 
+        if(target < arr[mid]){ 
+            if(mid > 0 && target > arr[mid - 1]) 
+                return getClosest(arr[mid - 1], arr[mid], target); 
+  			j = mid; 
         } 
-  
-        // If target is greater than mid 
-        else { 
-            if (mid < n - 1 && target < arr[mid + 1]) 
-                return getClosest(arr[mid], 
-                                  arr[mid + 1], target); 
-            // update i 
+        else{ 
+            if(mid < n - 1 && target < arr[mid + 1]) 
+                return getClosest(arr[mid], arr[mid + 1], target); 
             i = mid + 1;  
         } 
     } 
-  
-    // Only single element left after search 
     return arr[mid]; 
 }
 
 
 int main(){
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
   	cin.tie(0); 
 	int nr,ng,nb,t,a,bb,c;
 	cin>>t;
@@ -78,29 +60,27 @@ int main(){
 			cin>>g[i];
 		forn(i,nb)
 			cin>>b[i];
+		sort(r.begin(),r.end());
+		sort(g.begin(),g.end());
+		sort(b.begin(),b.end());
 		long long ans=LLONG_MAX;
 		forn(i,nr){
 			a=r[i];
 			bb = findClosest(g,ng,a);
 			c=findClosest(b,nb,a);
-			cout<<"ABC"<<a<<" "<<bb<<" "<<c<<"\n";
-			ans=min(ans,1.0*(a-bb)*(a-bb)+(bb-c)*(bb-c)+(c-a)*(c-a));
+			ans=min(ans,cal(a,bb,c));
 		}
-		cout<<"ans"<<ans<<"\n";
 		forn(i,ng){
 			a=g[i];
 			bb=findClosest(r,nr,a);
 			c=findClosest(b,nb,a);
-			cout<<"ABC"<<a<<" "<<bb<<" "<<c<<"\n";
-			ans=min(ans,1.0*(a-bb)*(a-bb)+(bb-c)*(bb-c)+(c-a)*(c-a));
+			ans=min(ans,cal(a,bb,c));
 		}
-		cout<<"ans"<<ans<<"\n";
 		forn(i,nb){
 			a=b[i];
 			bb=findClosest(g,ng,a);
 			c=findClosest(r,nr,a);
-			cout<<"ABC"<<a<<" "<<bb<<" "<<c<<"\n";
-			ans=min(ans,1.0*(a-bb)*(a-bb)+(bb-c)*(bb-c)+(c-a)*(c-a));
+			ans=min(ans,cal(a,bb,c));
 		}
 		cout<<ans<<endl;
 	}
