4b203891a9b54457f393301a8f88042bed03392d
harkiratdhanoa <harkirat2016@gmail.com>
2020-04-21 07:50:29 +0530

    helpful ideas

---
+++ hackerearth/bit-man/maximizing-expressions.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i, n) for(int i = 0; i < n; ++i)
+
+int main(){
+	int n,temp;
+	long long sum=0;
+	cin>>n;
+	vector<int> x(n);
+
+	forn(i,n)
+		cin>>x[i];
+
+	forn(i,n){
+		cin>>temp;
+		x[i]^=temp;
+	}
+
+	forn(i,n){
+		cin>>temp;
+		sum+=x[i]^temp&~x[i];
+	}
+
+	cout<<sum;
+}
