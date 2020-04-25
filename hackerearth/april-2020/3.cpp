e758e96695f2c9796c4e264c2c1fd53194b8db5a
harkiratdhanoa <harkirat2016@gmail.com>
2020-04-26 00:47:32 +0530

    April Circuits 2020

--- hackerearth/april-2020/3.cpp
+++ hackerearth/april-2020/3.cpp
@@ -1,3 +1,4 @@
+#pragma GCC optimize (Ofast)
 #include<bits/stdc++.h>
 using namespace std;
 #define forn(i,n) for(int i=0;i<n;i++)
@@ -6,14 +7,16 @@ int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	int n,m,ans=0;
+	int n,m,aa,ans=0;
 	vector<int>::iterator it;
 	
 	cin>>n>>m;
 	vector<int> a(n),b(m);
 
-	forn(i,n)
-		cin>>a[i];
+	forn(i,n){
+		cin>>aa;
+		a[i]=-aa;
+	}
 
 	forn(i,m)
 		cin>>b[i];
@@ -23,19 +26,16 @@ int main(){
 
 	while(a.size()>0){
 		ans++;
-		forn(j,m){
-			it = lower_bound(a.begin(), a.end(), b[j]); 
-			cout<<*it<<" ";
-			if(it==a.end()){
-				cout<<"oops";
-				break;
-			}
-			a.erase(--it);
-			
+		if(b[0]<-a[0]){
+			ans=0;
+			break;
 		}
-		cout<<ans<<"\n";
+		forn(j,m){
+			it = lower_bound(a.begin(), a.end(), -b[j]); 
+			if(it==a.end())
+				break;
+			a.erase(it);
+		}
 	}
-
 	cout<<2*ans-1<<"\n";
-
 }
