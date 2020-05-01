01959f75bb741341fcde1eaf7b3ae669c7d8108c
harkiratdhanoa <harkirat2016@gmail.com>
2020-05-01 17:51:08 +0530

    ladder1500-1599 72

--- a2oj/ladder1500-1599/72.cpp
+++ a2oj/ladder1500-1599/72.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 #define forn(i,n) for(int i=0;i<n;i++)
 
-vector<int> v(10000001,0);
+vector<int> v(10000000,0);
 
-int f(int n){
-	if(n==2)
-		return 3;
-	return f(n-1);
-}
 int main(){
-	int n,a;
+	long long n,power = 3;
 	cin>>n;
-	cout<<f(n);
+	v[1]=3;
+	forn(i,n)
+		if(i>=2){
+			v[i]=(v[i-2]+2*power)%1000000007;
+			power=power*3%1000000007;
+		}
+	cout<<v[n-1];
 	return 0;
 } 
