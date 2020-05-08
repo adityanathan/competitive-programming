7aabbf3d3dde8840e61324a9285c12a4705840b7
harkiratdhanoa <harkirat2016@gmail.com>
2020-05-08 23:58:20 +0530

    May Long 2020

--- codechef/long/may-2020/c.cpp
+++ codechef/long/may-2020/c.cpp
@@ -3,18 +3,51 @@
 #include<unordered_map>
 using namespace std;
 #define forn(i, n) for(int i = 0; i < n; ++i)
-#define forit(it, m) for(auto it = m.begin(); it!=m.end(); ++it)
 
 int main(){
 	ios::sync_with_stdio(false);
   	cin.tie(0); 
-	int n,t;
+	int n,i,t,k;
+	bool f;
 	cin>>t;
 
 	while(t--){
-		cin>>n;
+		cin>>n>>k;
+		vector<int> a(n);
+		unordered_map<int,int> m;
+		vector<string> ans;
+		forn(i,n){
+			cin>>a[i];
+			m[--a[i]]=i;
+		}
+		while(k>0 && !f){
+			f=true;
+			for(i=0;i<n;i++){
+				if(a[i]==i)
+					continue;
+				if(a[a[i]]==i)
+					break;
+				ans.push_back(to_string(i+1)+" "+to_string(a[i]+1)+" "+to_string(m[i]+1));
+				k--;
+				m[a[a[i]]]=m[i];
+				m[i]=i;
+				m[a[i]]=a[i];
+				a[m[i]]=a[a[i]];
+				a[a[i]]=a[i];
+				a[i]=i;
+			}
+			forn(i,n)
+				if(a[i]!=i)
+					f=false;
+		}
 		
-		cout<<n<<endl;
+		if(!f){
+			cout<<"-1\n";
+			continue;
+		}
+		cout<<ans.size()<<"\n";
+		forn(i,ans.size())
+			cout<<ans[i]<<endl;
 	}
 	return 0;
 }
