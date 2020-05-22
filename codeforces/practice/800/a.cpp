938be849aa54647a11f6990865658dee4561ddb0
harkiratdhanoa <harkirat2016@gmail.com>
2020-05-22 17:04:05 +0530

    practice: 800 ABCDEFGHIJ

---
+++ codeforces/practice/800/a.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "]: [", _print(x)
+#else
+#define debug(x...) 141
+#endif
+
+#define forn(i,x,n) for(int i = x; i < n; ++i)
+#define forit(it, m) for(decltype(m)::iterator it = m.begin(); it!=m.end(); ++it)
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,t,a;
+    t=1;
+
+    while(t--){
+        cin>>n;
+        map<int,int> m;
+        forn(i,0,n){
+        	cin>>a;
+        	m[a]++;
+        }
+        int ans=0;
+        forit(it,m){
+        	ans=max(ans,it->second);
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
