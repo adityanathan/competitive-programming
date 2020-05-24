cf48aedd0c90e374994fe8f4a1c55bd2663c2289
harkiratdhanoa <harkirat2016@gmail.com>
2020-05-24 23:49:06 +0530

    DS_Store file modified

--- atcoder/debug.h
+++ atcoder/debug.h
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 void __print(int x) {cerr << x;}
 void __print(long x) {cerr << x;}
 void __print(long long x) {cerr << x;}
@@ -22,35 +19,4 @@ void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ 
 void _print() {cerr << "]\n";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
 #define debug(x...) cerr << "[" << #x << "]: [", _print(x)
-#else
-#define debug(x...) 141
-#endif
-
-#define forn(i,x,n) for(int i = x; i < n; ++i)
-#define forit(it, m) for(decltype(m)::iterator it = m.begin(); it!=m.end(); ++it)
-typedef long long ll;
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n,t=1;
-
-    {
-        cin>>n;
-        if(n%2==0)
-        	cout<<"-1\n";
-        else{
-    		forn(i,0,n)
-    			cout<<i<<" ";
-    		cout<<"\n";
-    		forn(i,0,n)
-    			cout<<i<<" ";
-    		cout<<"\n";
-    		forn(i,0,n)
-    			cout<<(2*i)%n<<" ";
-        }
-    }
-    return 0;
-}
