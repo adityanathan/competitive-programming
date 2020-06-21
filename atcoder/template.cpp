f82e533bacfa883c42ffce422ef28caaacf2b786
harkiratdhanoa <harkirat2016@gmail.com>
2020-06-21 21:28:25 +0530

    modifying template file (do not include multiple test cases by default)

--- atcoder/template.cpp
+++ atcoder/template.cpp
@@ -13,12 +13,10 @@ typedef long long ll;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n,t;
-    cin>>t;
+    int n;
 
-    while(t--){
-        cin>>n;
-        cout<<n<<'\n';
-    }
+    cin>>n;
+    cout<<n<<'\n';
+    
     return 0;
 }
