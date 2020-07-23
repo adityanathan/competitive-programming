6a2a71ea6bbf568c628f4275d47ac5c151cca5ab
Harkirat Dhanoa <harkirat2016@gmail.com>
2020-07-23 20:27:11 +0530

    practice: educational round 11 ABCDF

--- codeforces/practice/edu#11/f.cpp
+++ codeforces/practice/edu#11/f.cpp
@@ -15,26 +15,24 @@ int main(){
     cin.tie(0);
     ll n, ans=0;
     cin>>n;
+    vector<ll> a(n),sum(n),score(n);
+    cin>>a[0];
+    sum[0]=score[0]=a[0];
+    forn(i,1,n){
+        cin>>a[i];
+        sum[i]=a[i]+sum[i-1];
+        score[i]=(i+1)*a[i]+score[i-1];
+    }
 
-    int x[n], y[n];
-    map<pair<int,int>,ll> m;
+    ll max_so_far=0, max_ending_here=0;
 
-    forn(i,0,n)
-    	cin>>x[i]>>y[i];
+    forn(i,0,n){ 
+       max_ending_here = max_ending_here+a[i]; 
+       if (max_ending_here<0)
+           max_ending_here=0;
 
-    forn(i,0,n)
-    	forn(j,i+1,n)
-    		if(y[j]>y[i])
-    			m[{x[j]-x[i],y[j]-y[i]}]++;
-    		else if(y[j]==y[i])
-    			m[{abs(x[j]-x[i]),0}]++;
-    		else
-    			m[{x[i]-x[j],y[i]-y[j]}]++;
-
-    forit(it,m)
-    	ans+=it->second*(it->second-1)/2;
-
-    cout<<ans/2;
-    
+       else if(max_so_far < max_ending_here) 
+           max_so_far = max_ending_here;
+    }
     return 0;
 }
