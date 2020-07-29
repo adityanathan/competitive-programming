4a3228d861dd956f48e131f242c6d6c6a0ab5ce5
Harkirat Dhanoa <harkirat2016@gmail.com>
2020-07-29 20:14:48 +0530

    practice: educational round 76 ABCD

--- codeforces/practice/edu#76/d.cpp
+++ codeforces/practice/edu#76/d.cpp
@@ -13,11 +13,23 @@ typedef long long ll;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n,t;
+    int n,t,m;
     cin>>t;
 
     while(t--){
         cin>>n;
+        vector<int> a(n);
+        map<int,set<int>> m;
+        forn(i,0,n)
+        	cin>>a[i];
+        cin>>m;
+        forn(i,0,m)
+        	cin>>p>>s, m[p].insert(s);
+        int i=0, pw=0;
+        while(i<n){
+        	pw=max(pw,a[i]);
+        	if(pw)
+        }
         cout<<n<<'\n';
     }
     return 0;
