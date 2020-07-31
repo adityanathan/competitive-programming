9995b8eff03a3171c31eaa8bba64417198aea4d2
Harkirat Dhanoa <harkirat2016@gmail.com>
2020-07-31 23:29:56 +0530

    Modifying contest template code

--- codeforces/contest.sh
+++ codeforces/contest.sh
@@ -1,20 +1,14 @@
 cd /Users/harkiratdhanoa/Desktop/iitd/competitive/codeforces
-
 if [ $# -ne 0 ]; 
 then	if ! [ -d "$1" ]; 
 	then
 		mkdir $1
 		cd $1
-
-
 		cat /Users/harkiratdhanoa/Desktop/iitd/competitive/codeforces/template.cpp 		| tee -a a.cpp b.cpp c.cpp d.cpp > /dev/null
-
 		# subl a.cpp
 	else
 		cd $1
 	fi
-
 fi
-
 clear
 open .
