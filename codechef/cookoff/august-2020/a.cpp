5d6165f344616d11105e7b9a254a2156e1b37771
Harkirat Dhanoa <harkirat2016@gmail.com>
2020-08-23 23:02:34 +0530

    August Cook-Off 2020 (Div 1)

---
+++ codechef/cookoff/august-2020/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "../debug.h"
+#else
+#define debug(x...) 141
+#endif
+#define forn(i,x,n) for(int i = x; i < n; ++i)
+#define forit(it, m) for(auto it = m.begin(); it!=m.end(); ++it)
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll n,t,x,y,ans;
+    cin>>t;
+
+    while(t--){
+        cin>>n;
+        ans=0;
+        forn(i,0,n)
+        	cin>>x>>y;
+        while(n>2)
+        	ans+=n, n/=2;
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
