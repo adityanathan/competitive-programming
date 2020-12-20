4ff76a57dcfb5c06ef54c4e773dfbc9075be5de5
Harkirat Dhanoa <harkirat2016@gmail.com>
2020-12-20 23:53:18 +0530

    round 692 (div 2) ABC

--- codeforces/#692/b.cpp
+++ codeforces/#692/b.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #ifdef LOCAL
 #include "../debug.h"
 #else
@@ -9,23 +9,23 @@ using namespace std;
 #define forn(i,x,n) for(int i = x; i < n; ++i)
 #define forit(it, m) for(auto it = m.begin(); it!=m.end(); ++it)
 typedef long long ll;
-
+ 
 bool check(ll n){
-	set<int> s;
-	ll x=n;
-	while(x)
-		s.insert(x%10), x/=10;
-	for(auto x:s)
-		if(x&&n%x)
-			return 0;
-	return 1;
+    ll x=n;
+    while(x)
+        if(x%10 && n%(x%10))
+            return 0;
+        else
+            x/=10;
+    
+    return 1;
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     ll n,t;
     cin>>t;
-
+ 
     while(t--){
         cin>>n;
         while(!check(n++));
