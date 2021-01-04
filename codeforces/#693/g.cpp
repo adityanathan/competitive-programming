f4bb9a3aca00735486573679eafc72133bf77bdf
Harkirat Dhanoa <harkirat2016@gmail.com>
2021-01-05 03:50:57 +0530

    round 693 (div 3) ABCDG

--- codeforces/#693/g.cpp
+++ codeforces/#693/g.cpp
@@ -7,35 +7,23 @@ using namespace std;
 #define debug(x...) 141
 #endif
 #define forn(i,x,n) for(int i = x; i < n; ++i)
-#define forit(it, m) for(auto it = m.begin(); it!=m.end(); ++it)
+#define forit(it, m) for(auto it = m.rbegin(); it!=m.rend(); ++it)
 typedef long long ll;
 
-vector<bool> vis;
 vector<int> dist, ans;
 vector<vector<int>> adj;
-
-void dfs(int u){
-    vis[u]=true;
-    for(auto v:adj[u]){
-        if(!vis[v])
-            dfs(v);
-        if(dist[u]<dist[v])
-            ans[u]=min(ans[u],ans[v]);
-    }
-}
 
 void bfs(int s){
     queue<int> q;
     q.push(s);
     dist[s]=0;
-    vis[s] = true;
 
     while(!q.empty()){
         int u = q.front();
         q.pop();
         for(auto v: adj[u])
-            if(!vis[v])
-                q.push(v), dist[v]=dist[u]+1, vis[v]=true;
+            if(dist[v]==1e9)
+                q.push(v), dist[v]=dist[u]+1;
             else if(dist[v]<dist[u])
                     ans[u]=min(ans[u],dist[v]);
     }
@@ -50,7 +38,6 @@ int main(){
     while(t--){
         cin>>n>>m;
         adj.assign(n,vector<int>());
-        vis.assign(n,0);
         dist.assign(n,1e9);
         ans.assign(n,1e9);
         forn(i,0,m){
@@ -58,11 +45,17 @@ int main(){
             adj[x-1].push_back(y-1);
         }
         bfs(0);
-        vis.assign(n,0);
-        debug(dist,ans);
-        dfs(0);
+        map<int,vector<int>> lvl;
         forn(i,0,n)
-            cout<<min(dist[i],ans[i])<<' '; cout<<'\n';
+            lvl[dist[i]].push_back(i);
+        forit(it,lvl)
+            for(auto u:it->second)
+                for(auto v:adj[u])
+                    if(dist[u]<dist[v])
+                        ans[u]=min(ans[u],ans[v]);
+        forn(i,0,n)
+            cout<<min(dist[i],ans[i])<<' ';
+        cout<<'\n';
     }
     return 0;
 }
